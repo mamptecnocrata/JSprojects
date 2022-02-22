90048259e03fa5f40ce81b9cf945da9f6f7504ab
Miguel Matos <matospmiguela@gmail.com>
2022-02-22 15:27:48 -0400

    Anexando el code para insertar las agujas del reloj

--- 01_analogue_clock/clock.js
+++ 01_analogue_clock/clock.js
@@ -14,6 +14,8 @@ function drawClock() {
 	drawFace(ctx, radius);
 	// chapter 3
 	drawNumbers(ctx, radius);
+	// chapter 4
+	drawTime(ctx,radius);
 }
 
 function drawFace(ctx, radius) {
@@ -51,4 +53,34 @@ function drawNumbers(ctx,radius) {
 		ctx.translate(0, radius*0.85);
 		ctx.rotate(-ang);
 	}
+}
+
+function drawTime(ctx, radius) {
+	var now = new Date();
+	var hour = now.getHours();
+	var minute = now.getMinutes();
+	var second = now.getSeconds();
+
+	hour=hour%12;
+	hour=(hour*Math.PI/6)+
+	(minute*Math.PI/(6*60))+
+	(second*Math.PI/(360*60));
+	drawHand(ctx, hour, radius*0.5, radius*0.07);
+
+	minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+	drawHand(ctx, minute, radius*0.8, radius*0.07);
+
+	second=(second*Math.PI/30);
+	drawHand(ctx, second, radius*0.9, radius*0.02);
+}
+
+function drawHand(ctx, pos, length, width) {
+	ctx.beginPath();
+	ctx.lineWidth = width;
+	ctx.lineCap = "round";
+	ctx.moveTo(0,0);
+	ctx.rotate(pos);
+	ctx.lineTo(0, -length);
+	ctx.stroke();
+	ctx.rotate(-pos);
 }
